4d32e0652bbc662e11dcae85a4f461d7e469ecd6
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-02-01 21:33:10 +0300

    Implementing filter mechanism

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,28 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 
 const HomePage = () => {
+  const [search, setSearch] = useState('');
   const northAmericaCountries = useSelector((state) => state.continentReducer);
+  const filteredCountries = northAmericaCountries.filter((country) => {
+    const newSearch = country.country.toLowerCase().includes(search);
+    return newSearch;
+  });
   return (
     <>
       <Header />
       <div className="MainContainer">
+        <input className="search" placeholder="Search Country ..." value={search} onChange={(e) => setSearch(e.target.value)} />
         <section className="CountryContainer">
-          {northAmericaCountries.map((item) => (
+          {filteredCountries.map((item) => (
             <Link
               key={item.id}
               to={`/country/${item.country}`}
               className="Container"
             >
-              <h2>{item.country}</h2>
+              <h2 className="Name">{item.country}</h2>
               <img src={item.countryFlag} alt="country flag" />
               <h2>{item.id}</h2>
             </Link>
