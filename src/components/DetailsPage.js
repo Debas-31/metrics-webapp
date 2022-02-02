28388a0169cd59d30be113e16865f68180ba59c4
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-02-02 16:57:10 +0300

    Add styling

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { fetchCovidDataByCountry } from '../redux/Api';
-import Header from './Header';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { fetchCovidDataByCountry } from "../redux/Api";
+import Header from "./Header";
 
 const DetailsPage = () => {
   const { name } = useParams();
@@ -15,34 +15,35 @@ const DetailsPage = () => {
   }, []);
   return (
     <>
-      {
-      state ? (
-        <div>
-          <Header />
-          <img src={state.countryInfo.flag} alt="country flag" />
-          <h2 className="Name">{state.name}</h2>
-          <h2 className="Name">
-            <span>Population: </span>
-            {state.population.toLocaleString()}
-          </h2>
-          <h2 className="Name">
-            <span>Class: </span>
-            {state.cases.toLocaleString()}
-          </h2>
-          <h2 className="Name">
-            <span>Active: </span>
-            {state.active.toLocaleString()}
-          </h2>
-          <h2 className="Name">
-            <span>Tests: </span>
-            {state.tests.toLocaleString()}
-          </h2>
-          <h2>{}</h2>
+      {state ? (
+        <div className="DetailContainer">
+          <div>
+            <Header />
+            <h2 className="Country">{state.country}</h2>
+            <img src={state.countryInfo.flag} alt="country flag" />
+            <h2 className="Name">{state.name}</h2>
+            <h2 className="Name">
+              <span>Population : </span>
+              {state.population.toLocaleString()}
+            </h2>
+            <h2 className="Name">
+              <span>Class : </span>
+              {state.cases.toLocaleString()}
+            </h2>
+            <h2 className="Name">
+              <span>Active : </span>
+              {state.active.toLocaleString()}
+            </h2>
+            <h2 className="Name">
+              <span>Tests : </span>
+              {state.tests.toLocaleString()}
+            </h2>
+            <h2>{}</h2>
+          </div>
         </div>
       ) : (
         <div>Loading</div>
-      )
-      }
+      )}
     </>
   );
 };
